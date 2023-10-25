0037e12971ae22a8a43e9cf98a11b80d89c15d8e
Spike184 <ilakin.r@gmail.com>
2023-10-25 15:46:19 +0300

    Fix: Header + Content + Footer again

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/Appp.js</code> an sve to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://youtu.be/dQw4w9WgXcQ?si=TA4xkOhNw6RngRQF"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="Header">
+        <p>header</p>
+      </div>
+
+      <div className='Content'>
+        <p>Content</p>
+      </div>
+
+      <div className='Footer'>
+        <p>Footer</p>
+      </div>
+
     </div>
   );
 }
