0be7ad731c3ce0ae6be7b1e163ce17863ef40871
CONMIROPC\redli <tmb.command@gmail.com>
2023-11-12 01:06:42 +0300

    feat(move files)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
-import Footer from './Components/Footer';
-import ContentMainPage from './Components/ContentMainPage';
+import Footer from './Components/Footer/Footer';
+import ContentMainPage from './Components/Body/ContentMainPage';
 
 function App() {
   return (
