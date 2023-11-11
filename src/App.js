4ec67e0e7e461d102c96eef5e75089e3b2507209
CONMIROPC\redli <tmb.command@gmail.com>
2023-11-12 02:13:25 +0300

    feat:footer ground create

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
+import Footer from './Components/Footer_Main/Footer_Main';
 import ContentMainPage from './Components/Body/ContentMainPage';
 
 function App() {
