a9cb9787f7eae9132e4de58cf50d8133c4355e22
Spike184 <ilakin.r@gmail.com>
2023-11-12 13:16:34 +0300

    Fix: Header_Logo  's' -> 'style'

--- src/Components/Header_Main/Header_Logo/Header_Logo.jsx
+++ src/Components/Header_Main/Header_Logo/Header_Logo.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import s from '../Header_Logo/Header_Logo.module.css';
+import style from '../Header_Logo/Header_Logo.module.css';
 
 const Header_Logo = () => {
     
     return(
-    <div className={s.Header_Logo}>
+    <div className={style.Header_Logo}>
         <a href='#'>EmigrantRate</a>
     </div>
     )
