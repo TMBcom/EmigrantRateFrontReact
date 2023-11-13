4054f3d8c57dff66d6a8ab341d2bc00c691ed765
CONMIROPC\redli <tmb.command@gmail.com>
2023-11-13 16:32:17 +0300

    feat:link underline animation

--- src/Components/Header_Main/Header_Logo/Header_Logo.jsx
+++ src/Components/Header_Main/Header_Logo/Header_Logo.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import style from './Header_Logo.module.css';
-
+import {Link} from "react-router-dom";
 
 function Header_Logo() {
     return(
       <div className={style.Header_Logo}>
-       Logo
+       <Link to='/MainPage'>Login</Link>
       </div>
 
     );
