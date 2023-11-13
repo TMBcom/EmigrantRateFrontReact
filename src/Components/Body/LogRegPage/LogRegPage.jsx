f7f012448807267f599e84a18c3f1502f7bc17f0
CONMIROPC\redli <tmb.command@gmail.com>
2023-11-14 01:00:35 +0300

    fix:page struct

--- src/Components/Body/LogRegPage/LogRegPage.jsx
+++ src/Components/Body/LogRegPage/LogRegPage.jsx
@@ -3,17 +3,13 @@ import style from './LogRegPage.module.css';
 import LoginMain from './LoginMain/LoginMain';
 import RegistrationMain from './RegistrationMain/RegistrationMain';
 import {Routes, Route} from "react-router-dom";
-import EmployersPage_Main from './../SearchPage/EmployersPage_Main/EmployersPage_Main';
 
 function LogRegPage() {
     return(
     <div className={style.LogReg}>
     <Routes>
         <Route path="/LoginPage" element={<LoginMain />}/>
-        <Route path="/EmigrantPage" element={<EmigrantPage_Main />}/>
         <Route path="/RegistrationPage" element={<RegistrationMain />}/>
-        <Route path="/EmployerPage" element={<EmployersPage_Main/>}/>
-
     </Routes>
         </div>
 
