93cb2c9e01764955446c53686de8722259030cc6
mh_mnkn <misha.minkin2002@mail.ru>
2023-11-13 19:49:45 +0300

    feat LogRegPage,EmigrantPage_Main-emigrant in body

--- src/Components/Body/LogRegPage/LogRegPage.jsx
+++ src/Components/Body/LogRegPage/LogRegPage.jsx
@@ -3,12 +3,14 @@ import style from './LogRegPage.module.css';
 import LoginMain from './LoginMain/LoginMain';
 import RegistrationMain from './RegistrationMain/RegistrationMain';
 import {Routes, Route} from "react-router-dom";
+import EmigrantPage_Main from './../SearchPage/EmigrantPage_Main/EmigrantPage_Main';
 
 function LogRegPage() {
     return(
     <div className={style.LogReg}>
     <Routes>
         <Route path="/LoginPage" element={<LoginMain />}/>
+        <Route path="/EmigrantPage" element={<EmigrantPage_Main />}/>
         <Route path="/RegistrationPage" element={<RegistrationMain />}/>
     </Routes>
         </div>
