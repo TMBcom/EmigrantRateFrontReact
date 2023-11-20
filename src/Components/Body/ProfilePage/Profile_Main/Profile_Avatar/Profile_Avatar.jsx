9cb015b6379d2bbe3c3b2bf4a86b9f5419f5f507
CONMIROPC\redli <tmb.command@gmail.com>
2023-11-21 01:05:49 +0300

    feat:reviews ground

--- src/Components/Body/ProfilePage/Profile_Main/Profile_Avatar/Profile_Avatar.jsx
+++ src/Components/Body/ProfilePage/Profile_Main/Profile_Avatar/Profile_Avatar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import style from './Profile_Avatar.module.css';
-
+import emi from './emi_1.jpg'
 
 function Profile_Avatar() {
     return(
-      <div className={style.Profile_Avatar}>
-       
-      </div>
+       <div className={style.Profile}>
+              <img className={style.ProfileAvatar} src={emi} alt="user" width="300"/>
+              <div className={style.ProfileName}>ФИО</div>
+              <div className={style.Status}>Статус</div>
+              <div className={style.Rate}>Рейтинг</div>
+          </div>
 
     );
 }
--- src/Components/Body/ProfilePage/Profile_Main/Profile_Reviews/Profile_Reviews.jsx
+++ src/Components/Body/ProfilePage/Profile_Main/Profile_Reviews/Profile_Reviews.jsx
@@ -4,9 +4,17 @@ import style from './Profile_Reviews.module.css';
 
 function Profile_Reviews() {
     return(
-      <div className={style.Profile_Reviews}>
-       
+      <div className={style.fb_form}>
+      <form action='#' className={style.form_group}>
+      <h2>Что вы думаете о работнике?</h2>
+      <div className={style.input_review}>
+      <input className={style.form_control} placeholder='Ваше имя' type='text'/>
+      <input className={style.form_control} placeholder='Email' type='text'/>
       </div>
+      <textarea className={style.form_control} name='review' placeholder='Напишите что вы думаете' rows="10" cols="45"></textarea>
+      <input className={style.btn_primary} type='submit'/>
+      </form>
+          </div>
 
     );
 }
